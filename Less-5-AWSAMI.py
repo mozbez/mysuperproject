8738397d1f4e47dcb5c9f9e6c852664cf028d363
Михаил В. Ожерельев <C:\Users\ozherelev\AppData\Roaming\The Bat!>
2025-11-17 09:33:28 +0300

    Added Less5

---
+++ Less-5-AWSAMI.py
@@ -0,0 +1,18 @@
+
+#!/bin/python3
+#-----------------------------------------------
+# Program by Michael.Oz.
+#
+#
+# Version    Date          Info
+# 1.0        2025      Initial Version
+# 1.1        2025      New fixes
+#-----------------------------------------------
+
+import shutil   # For Copyfile
+import os       # For GetFileSize and Check it File exist
+impot sys       # For CLI Arguments
+
+# lesson-4-putgelog.py mylog.txt 10 5
+
+print("hallo")
